5d8957a57f23db41afbcfabab3531339517c7494
Pankaj Yadav <118116376+Pankajkantghz@users.noreply.github.com>
2025-08-05 15:21:28 +0530

    Update AuthImagePattern.jsx

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,13 +1,13 @@
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
-      <div className="max-w-md text-center">
-        <div className="grid grid-cols-3 gap-3 mb-8">
-          {[...Array(9)].map((_, i) => (
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-20 pb-10 ">
+      <div className="max-w-md text-center m-10">
+        <div className="grid grid-cols-5 gap-10 m-4">
+          {[...Array(5)].map((_, i) => (
             <div
               key={i}
-              className={`aspect-square rounded-2xl bg-primary/10 ${
-                i % 2 === 0 ? "animate-pulse" : ""
+              className={`aspect-square rounded-2xl bg-primary/80 ${
+                i % 2 === 0 ? "animate-bounce" : ""
               }`}
             />
           ))}
